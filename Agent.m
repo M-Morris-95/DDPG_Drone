7508cf15ecaabda57f24dd7e54d00a182d1b8c5e
Michael Morris <49106997+Big-Beef@users.noreply.github.com>
2019-04-08 15:25:07 +0100

    Add files via upload

--- Agent.m
+++ Agent.m
@@ -1,22 +1,22 @@
 mdl = 'Tricopter';
+%https://harikrishnansuresh.github.io/assets/deep-rl-final.pdf
 open_system(mdl)
 
 % Specify observer information
-obsInfo = rlNumericSpec([6 1],...
-    'LowerLimit',[-1 -1 -1 -1 -1 -1]',...
-    'UpperLimit',[ 1  1  1  1  1  1]');
+obsInfo = rlNumericSpec([2 1],...
+    'LowerLimit',[-1 -1]',...
+    'UpperLimit',[ 1  1]');
 obsInfo.Name = 'observer';
-obsInfo.Description = 'dist X to target, dist Y to target, dist  Z to target,prev dist Z to target,prev dist Y to target,prev dist  Z to target';
+obsInfo.Description = 'yaw to target, pitch to target, dist to target';
 numObs = obsInfo.Dimension(1);
 
 % Specify actor information
 actInfo = rlNumericSpec([3 1],...
-    'LowerLimit',[-1 -1 -1]',...
-    'UpperLimit',[ 1  1  1]');
+    'LowerLimit',-1',...
+    'UpperLimit', 1');
 actInfo.Name = 'actor';
-obsInfo.Description = 'X_vel,Y_vel,Z_vel';
+obsInfo.Description = 'Thr, roll, pitch, Yaw';
 numAct = actInfo.Dimension(1);
-
 
 % Initialise environment - simulator, agent, observer information, actor
 % information
@@ -25,68 +25,60 @@ env = rlSimulinkEnv('Tricopter','Tricopter/RL Agent',...
 
 % Set model params
 Ts = 0.4; % sample time
-Tf = 80;  % finish time
+Tf = 40;  % finish time
 rng('shuffle');    % Set rng seed
 
 % Create some convolutional neural nets for the critic
 % specify the number of outputs for the hidden layers.
-hiddenLayerSize = 128; 
+hiddenLayerSize = 6; 
 
-observationPath = [
-    imageInputLayer([numObs 1 1],'Normalization','none','Name','observation')
-    fullyConnectedLayer(hiddenLayerSize,'Name','fc1')
-    reluLayer('Name','relu1')
-    fullyConnectedLayer(hiddenLayerSize,'Name','fc2')
-    additionLayer(2,'Name','add')
-    reluLayer('Name','relu2')
-    fullyConnectedLayer(hiddenLayerSize,'Name','fc3')
-    reluLayer('Name','relu3')
-    fullyConnectedLayer(1,'Name','fc4')];
+statePath = [
+    imageInputLayer([numObs 1 1], 'Normalization', 'none', 'Name', 'observation')
+    fullyConnectedLayer(hiddenLayerSize, 'Name', 'CriticStateFC1')
+    reluLayer('Name', 'CriticRelu1')
+    fullyConnectedLayer(hiddenLayerSize, 'Name', 'CriticStateFC2')];
 actionPath = [
-    imageInputLayer([numAct 1 1],'Normalization','none','Name','action')
-    fullyConnectedLayer(hiddenLayerSize,'Name','fc5')];
+    imageInputLayer([numAct 1 1], 'Normalization', 'none', 'Name', 'action')
+    fullyConnectedLayer(hiddenLayerSize, 'Name', 'CriticActionFC1', 'BiasLearnRateFactor', 0)];
+commonPath = [
+    additionLayer(2,'Name', 'add')
+    reluLayer('Name','CriticCommonRelu')
+    fullyConnectedLayer(1, 'Name', 'CriticOutput')];
 
-% create the layerGraph
-criticNetwork = layerGraph(observationPath);
+criticNetwork = layerGraph();
+criticNetwork = addLayers(criticNetwork,statePath);
 criticNetwork = addLayers(criticNetwork,actionPath);
-
-% connect actionPath to obervationPath
-criticNetwork = connectLayers(criticNetwork,'fc5','add/in2');
-criticOptions = rlRepresentationOptions('LearnRate',1e-03,'GradientThreshold',1,'UseDevice',"gpu");
+criticNetwork = addLayers(criticNetwork,commonPath);
+criticNetwork = connectLayers(criticNetwork,'CriticStateFC2','add/in1');
+criticNetwork = connectLayers(criticNetwork,'CriticActionFC1','add/in2');
+criticOptions = rlRepresentationOptions('LearnRate',1e-03,'GradientThreshold',1);
 critic = rlRepresentation(criticNetwork,obsInfo,actInfo,'Observation',{'observation'},'Action',{'action'},criticOptions);
-
-% figure
-% plot(criticNetwork)
 
 % Create the actor network (another CNN)
 actorNetwork = [
     imageInputLayer([numObs 1 1],'Normalization','none','Name','observation')
     fullyConnectedLayer(hiddenLayerSize,'Name','fc1')
     reluLayer('Name','relu1')
-    fullyConnectedLayer(hiddenLayerSize,'Name','fc2')
-    reluLayer('Name','relu2')
-    fullyConnectedLayer(hiddenLayerSize,'Name','fc3')
-    reluLayer('Name','relu3')
-    fullyConnectedLayer(numAct,'Name','fc4')
+    fullyConnectedLayer(numAct,'Name','fc2')
     tanhLayer('Name','tanh1')];
 
-actorOptions = rlRepresentationOptions('LearnRate',1e-04,'GradientThreshold',1,'UseDevice',"gpu");
+actorOptions = rlRepresentationOptions('LearnRate',1e-04,'GradientThreshold',1);
 actor = rlRepresentation(actorNetwork,obsInfo,actInfo,'Observation',{'observation'},'Action',{'tanh1'},actorOptions);
+
 
 % Create the DDPG agent
 agentOptions = rlDDPGAgentOptions(...
     'SampleTime',Ts  ,...
-    'TargetSmoothFactor',1e-3,...
-    'ExperienceBufferLength',1e6 ,...
+    'TargetSmoothFactor',0.125,...
+    'ExperienceBufferLength',5e3 ,...
     'DiscountFactor',0.98,...
-    'MiniBatchSize',64);
+    'MiniBatchSize',32);
 
 agentOpts.NumStepsToLookAhead = 16;
 agentOpts.NoiseOptions.InitialAction = 0;
 agentOpts.NoiseOptions.Mean = 0;
-agentOpts.NoiseOptions.Variance = 1e-3;
-agentOpts.NoiseOptions.VarianceDecayRate = 1e-2;
-
+agentOpts.NoiseOptions.Variance = 0.3;
+agentOpts.NoiseOptions.VarianceDecayRate = 0.6;
 agent = rlDDPGAgent(actor,critic,agentOptions);
 
 
@@ -99,7 +91,7 @@ trainOpts = rlTrainingOptions(...
     'Verbose', false, ...
     'Plots','training-progress',...
     'StopTrainingCriteria','AverageReward',...
-    'StopTrainingValue',800);
+    'StopTrainingValue',8000);
 
 disp('set up complete')
 doTraining = true;
